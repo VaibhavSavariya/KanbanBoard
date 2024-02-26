9b7bfac2bf31372fb8632dd1b0ec2ab93b539cd1
vaibhavs <vaibhav@greenapex.com>
2024-02-26 17:02:58 +0530

    Fixed UI of landing page

--- src/app/page.js
+++ src/app/page.js
@@ -16,46 +16,91 @@ const Home = () => {
   const isXs = useMediaQuery((theme) => theme.breakpoints.only("xs"));
   return (
     <>
-      <Container
-        // maxWidth=""
-        sx={{
-          mt: isXs ? 10 : 20,
-        }}
-      >
-        <Stack mb={6} spacing={2} alignItems={"center"} textAlign={"center"}>
-          <Typography
-            variant="h2"
+      {isXs ? (
+        <>
+          <Container
+            // maxWidth=""
             sx={{
-              fontWeight: "700",
+              mt: isXs ? 10 : 20,
             }}
           >
-            See the big picture with Todoist Boards Boards are a powerful.
-          </Typography>
-          {!isXs && (
-            <Typography
-              variant="body2"
-              sx={{
-                fontSize: "24px",
-              }}
+            <Stack
+              mb={6}
+              spacing={2}
+              alignItems={"center"}
+              textAlign={"center"}
             >
-              flexible way to organize your projects. Drag tasks between
-              sections, invite your teammates to join in, and visualize your
-              progress.
-            </Typography>
-          )}
-          <Button
-            onClick={() => {
-              router.push("/login");
+              <Typography
+                variant="h2"
+                sx={{
+                  fontWeight: "700",
+                }}
+              >
+                See the big picture with Todoist Boards Boards are a powerful.
+              </Typography>
+              {!isXs && (
+                <Typography
+                  variant="body2"
+                  sx={{
+                    fontSize: "24px",
+                  }}
+                >
+                  flexible way to organize your projects. Drag tasks between
+                  sections, invite your teammates to join in, and visualize your
+                  progress.
+                </Typography>
+              )}
+              <Button
+                onClick={() => {
+                  router.push("/login");
+                }}
+                size="large"
+                variant="contained"
+                color="primary"
+                endIcon={<ArrowForwardIosIcon />}
+              >
+                Get Started
+              </Button>
+            </Stack>
+          </Container>
+        </>
+      ) : (
+        <>
+          <img
+            src={
+              "https://atlassianblog.wpengine.com/wp-content/uploads/2024/01/kanban-101-final-1.png"
+            }
+            style={{
+              position: "absolute",
+              zIndex: -10,
+              height: "100vh",
+              width: "100vw",
             }}
-            size="large"
-            variant="contained"
-            color="primary"
-            endIcon={<ArrowForwardIosIcon />}
-          >
-            Get Started
-          </Button>
-        </Stack>
-      </Container>
+            alt="kanban board"
+          />
+
+          <Stack alignItems={"center"} textAlign={"center"}>
+            <Button
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                mt: 55,
+                backgroundColor: "#A978CD",
+                color: "white",
+              }}
+              onClick={() => {
+                router.push("/login");
+              }}
+              size="large"
+              variant="contained"
+              endIcon={<ArrowForwardIosIcon />}
+            >
+              Get Started
+            </Button>
+          </Stack>
+        </>
+      )}
     </>
   );
 };
