37a3c9f0a570252a7fb9b5514f5cfeb4a4e14ea0
vaibhavs <vaibhav@greenapex.com>
2024-02-29 15:02:19 +0530

    connection

--- src/db.js
+++ src/db.js
@@ -1,26 +1,15 @@
 import mongoose from "mongoose";
 
 const connect = async () => {
-  // Set strict query mode for Mongoose to prevent unknown field queries.
-  mongoose.set("strictQuery", true);
   try {
     await mongoose.connect(
       "mongodb://vaibhav:vaibhav1312@ac-r6kfvxo-shard-00-00.0tm31b4.mongodb.net:27017,ac-r6kfvxo-shard-00-01.0tm31b4.mongodb.net:27017,ac-r6kfvxo-shard-00-02.0tm31b4.mongodb.net:27017/?ssl=true&replicaSet=atlas-jdudka-shard-0&authSource=admin&retryWrites=true&w=majority&appName=KanbanCluster"
     );
-    const connection = mongoose.connection;
 
-    connection.on("connnected", () => {
-      console.log("Mongo Connect Successfully!");
-    });
-    connection.on("error", (err) => {
-      console.log(
-        "Mongo Connection Error, Please make sure Mongo is running",
-        err
-      );
-      process.exit();
-    });
+    console.log("MongoDB connected successfully!");
   } catch (error) {
-    console.log(error);
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // Exit the process with a non-zero status code
   }
 };
 
